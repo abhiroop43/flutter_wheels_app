5df02204ea95b9258fce239bf6ee20eb0dde4e7b
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2025-01-02 00:09:15 +0400

    feat: enhance vehicle details screen with image display and detailed information layout

--- lib/screens/vehicles_details_screen.dart
+++ lib/screens/vehicles_details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_wheels_app/models/vehicle.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class VehiclesDetailsScreen extends StatefulWidget {
   final Vehicle vehicle;
@@ -14,7 +15,7 @@ class _VehiclesDetailsScreenState extends State<VehiclesDetailsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Vehicle Details',
+        title: Text(widget.vehicle.name,
             style: TextStyle(
                 color: Theme.of(context).colorScheme.onPrimaryContainer)),
         actions: [
@@ -34,11 +35,68 @@ class _VehiclesDetailsScreenState extends State<VehiclesDetailsScreen> {
         ],
         backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       ),
-      body: Center(
-        child: Text(
-            '${widget.vehicle.name} details ${widget.vehicle.isFavorite}',
-            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                color: Theme.of(context).colorScheme.onPrimaryContainer)),
+      body: Container(
+        margin: EdgeInsets.symmetric(vertical: 4, horizontal: 6),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(widget.vehicle.imageUrl),
+              fit: BoxFit.cover,
+              height: 400,
+              width: double.infinity,
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  ListTile(
+                    title: Text('Brand'),
+                    trailing: Text(widget.vehicle.brand),
+                  ),
+                  ListTile(
+                    title: Text('Year'),
+                    trailing: Text(widget.vehicle.year.toString()),
+                  ),
+                  ListTile(
+                    title: Text('Price'),
+                    trailing: Text(
+                        '\$${widget.vehicle.priceInUsd.toStringAsFixed(2)}'),
+                  ),
+                  ListTile(
+                    title: Text('Fuel Type'),
+                    trailing: Text(widget.vehicle.fuelType.name.toUpperCase()),
+                  ),
+                  ListTile(
+                    title: Text('Transmission'),
+                    trailing: Text(widget.vehicle.transmissionType.name),
+                  ), // TODO: show English value
+                  ListTile(
+                    title: Text('Engine Capacity'),
+                    trailing:
+                        Text('${widget.vehicle.engineCapacityInLiters} L'),
+                  ),
+                  ListTile(
+                    title: Text('Seating Capacity'),
+                    trailing: Text('${widget.vehicle.seatingCapacity} seats'),
+                  ),
+                  ListTile(
+                    title: Text('Mileage'),
+                    trailing: Text('${widget.vehicle.mileageInKmpl} km/l'),
+                  ),
+                  ListTile(
+                    title: Text('Categories'),
+                    trailing: Text(widget.vehicle.categoryIds.join(', ')),
+                  ), // TODO: show actual category names
+                  ListTile(
+                    title: Text('Production Status'),
+                    trailing: Text(widget.vehicle.productionStatus.name),
+                  ), // TODO: show English value
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
