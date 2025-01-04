f49d240fc47c4e2f0bd59213b03a927df2a40471
Abhiroop Santra <abhiroop.santra@gmail.com>
2025-01-04 09:38:07 +0400

    feat: add favorites screen and integrate with bottom navigation bar

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_wheels_app/models/category.dart';
+import 'package:flutter_wheels_app/screens/favorites_screen.dart';
 import 'package:flutter_wheels_app/widgets/categories_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
@@ -8,6 +9,7 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<Category> categories = Category.getCategories();
+    int currentPageIndex = 0;
 
     return Scaffold(
         appBar: AppBar(
@@ -15,6 +17,27 @@ class CategoriesScreen extends StatelessWidget {
               style: TextStyle(
                   color: Theme.of(context).colorScheme.onPrimaryContainer)),
           backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+        ),
+        bottomNavigationBar: NavigationBar(
+          height: 64,
+          destinations: const <Widget>[
+            NavigationDestination(
+              icon: Icon(Icons.category_outlined),
+              label: 'Categories',
+              selectedIcon: Icon(Icons.category),
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.star_outline),
+              label: 'Favorites',
+              selectedIcon: Icon(Icons.star),
+            )
+          ],
+          selectedIndex: currentPageIndex,
+          onDestinationSelected: (int index) {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+              return FavoritesScreen();
+            }));
+          },
         ),
         body: GridView(
           padding: EdgeInsets.all(24),
